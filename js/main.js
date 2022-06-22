b8f0f0b0ebefc6df9f699c2c19d4793ddf5d6a45
uwuowoqwqqqq@gmail.com <uwuowoqwqqqq@gmail.com>
2022-06-22 20:48:54 +0300

    im change project

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var hamburgers = document.querySelectorAll(".hamburger");
+var menu = document.querySelector(".navigation");
 var forEach = function (t, o, r) {
   if ("[object Object]" === Object.prototype.toString.call(t))
     for (var c in t)
@@ -5,13 +7,13 @@ var forEach = function (t, o, r) {
   else for (var e = 0, l = t.length; l > e; e++) o.call(r, t[e], e, t);
 };
 
-var hamburgers = document.querySelectorAll(".hamburger");
 if (hamburgers.length > 0) {
   forEach(hamburgers, function (hamburger) {
     hamburger.addEventListener(
       "click",
       function () {
         this.classList.toggle("is-active");
+        menu.classList.toggle("open");
       },
       false
     );
